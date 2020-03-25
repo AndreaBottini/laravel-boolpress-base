e5e7215b08441d846592a4061ddbc596c0e5ff47
Andrea Bottini <andrea.bottini22@gmail.com>
2020-03-25 18:52:46 +0100

    Add index

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    @foreach ($posts as $post)
+    <ul>
+        <li>{{$post->title}}</li>
+        <li><img src="{{$post->img}}" alt=""></li>
+        <li>{{$post->author}}</li>
+        <li>{{$post->article}}</li>
+    </ul>
+    @endforeach
+    
+</body>
+</html>
